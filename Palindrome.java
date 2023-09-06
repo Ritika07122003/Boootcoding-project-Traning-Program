82c82d5fd3f9dc6f7ca7dd42866a091c97ef412c
Ritika07122003 <144015533+Ritika07122003@users.noreply.github.com>
2023-09-06 04:46:27 -0700

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,19 @@
+class Palindrome{  
+  public static void main(String args[]){  
+  int n = 1221;
+  int i=n;
+  int rev=0;
+  int number=0;
+  while(n !=0){
+	 
+  number = n % 10;
+  rev = (rev = 10)+number ;
+  n= n/10;
+  }
+  if(i==rev){
+  System.out.println("Nunber is Palindrome");
+  }else{
+  System.out.println("Number is not Palindrome");
+  }
+  }
+}
