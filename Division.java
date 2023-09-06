5d9bdea6b7bad1524f128a1589046062b2e27c6c
Ritika07122003 <144015533+Ritika07122003@users.noreply.github.com>
2023-09-06 04:41:39 -0700

    Create Division.java

---
+++ Division.java
@@ -0,0 +1,16 @@
+class Division{
+    public static void main(String[]args){
+	int number = 15;
+	
+	if(number % 3 == 0) {
+		System.out.println(number +" divisible by 3 ");
+	}else{
+		System.out.println(number +" not divisible by 3 ");
+	}
+	if(number % 5 == 0) {
+		System.out.println(number +" divisible by 5 ");
+	}else{
+		System.out.println(number +" not divisible by 5 ");
+	}
+}
+}
